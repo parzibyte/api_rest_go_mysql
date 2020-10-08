b4354b9017b12d32d40a3e71800bf604a526e8af
Luis Cabrera Benito <parzibyte@gmail.com>
2020-10-08 12:38:06 -0500

    Exponiendo videojuegos y videojuego por id

--- routes.go
+++ routes.go
@@ -2,22 +2,46 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func setupRoutes(enrutador *mux.Router) {
+func setupRoutes(router *mux.Router) {
 
-	enrutador.HandleFunc("/hola", func(w http.ResponseWriter, r *http.Request) {
-		respuesta := "Hola mundo"
-		_, err := getDB()
+	router.HandleFunc("/videogames", func(w http.ResponseWriter, r *http.Request) {
+		videoGames, err := getVideoGames()
 		if err == nil {
-			json.NewEncoder(w).Encode(respuesta)
+			respondWithSuccess(videoGames, w)
 		} else {
-			fmt.Println(err)
-			json.NewEncoder(w).Encode("Error conectando" + err.Error())
+			respondWithError(err, w)
+		}
+	}).Methods("GET")
+	router.HandleFunc("/videogame/{id}", func(w http.ResponseWriter, r *http.Request) {
+		idAsString := mux.Vars(r)["id"]
+		id, err := stringToInt64(idAsString)
+		if err != nil {
+			respondWithError(err, w)
+			// We return, so we stop the function flow
+			return
+		}
+		videogame, err := getVideoGameById(id)
+		if err != nil {
+			respondWithError(err, w)
+		} else {
+			respondWithSuccess(videogame, w)
 		}
 	}).Methods("GET")
 
+}
+
+// Helper functions for respond with 200 or 500 code
+func respondWithError(err error, w http.ResponseWriter) {
+	w.WriteHeader(http.StatusInternalServerError)
+	json.NewEncoder(w).Encode(err.Error())
+}
+
+func respondWithSuccess(data interface{}, w http.ResponseWriter) {
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(data)
 }
