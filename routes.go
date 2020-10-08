7d375e503489c517b260a1f86c7581f35fe77cd8
Luis Cabrera Benito <parzibyte@gmail.com>
2020-10-08 12:50:49 -0500

    API completa con todos los métodos del controlador

--- routes.go
+++ routes.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+//TODO: replace "GET" with http.MethodGet and so on
 func setupRoutes(router *mux.Router) {
 
 	router.HandleFunc("/videogames", func(w http.ResponseWriter, r *http.Request) {
@@ -32,6 +33,52 @@ func setupRoutes(router *mux.Router) {
 		}
 	}).Methods("GET")
 
+	router.HandleFunc("/videogame", func(w http.ResponseWriter, r *http.Request) {
+		// Declare a var so we can decode json into it
+		var videoGame VideoGame
+		err := json.NewDecoder(r.Body).Decode(&videoGame)
+		if err != nil {
+			respondWithError(err, w)
+		} else {
+			err := createVideoGame(videoGame)
+			if err != nil {
+				respondWithError(err, w)
+			} else {
+				respondWithSuccess(true, w)
+			}
+		}
+	}).Methods(http.MethodPost)
+
+	router.HandleFunc("/videogame", func(w http.ResponseWriter, r *http.Request) {
+		// Declare a var so we can decode json into it
+		var videoGame VideoGame
+		err := json.NewDecoder(r.Body).Decode(&videoGame)
+		if err != nil {
+			respondWithError(err, w)
+		} else {
+			err := updateVideoGame(videoGame)
+			if err != nil {
+				respondWithError(err, w)
+			} else {
+				respondWithSuccess(true, w)
+			}
+		}
+	}).Methods(http.MethodPut)
+	router.HandleFunc("/videogame/{id}", func(w http.ResponseWriter, r *http.Request) {
+		idAsString := mux.Vars(r)["id"]
+		id, err := stringToInt64(idAsString)
+		if err != nil {
+			respondWithError(err, w)
+			// We return, so we stop the function flow
+			return
+		}
+		err = deleteVideoGame(id)
+		if err != nil {
+			respondWithError(err, w)
+		} else {
+			respondWithSuccess(true, w)
+		}
+	}).Methods(http.MethodDelete)
 }
 
 // Helper functions for respond with 200 or 500 code
